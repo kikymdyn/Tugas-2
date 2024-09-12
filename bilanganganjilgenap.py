68391fc2384012debdde4bd53868c356aaa7c5e8
Muhammad Ribqi Madian Putra <rifkymadianp@gmail.com>
2024-09-12 13:51:31 +0800

    Create bilanganganjilgenap.py

---
+++ bilanganganjilgenap.py
@@ -0,0 +1,15 @@
+angka = int(input("Masukkan angka: "))
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap. ")
+else : 
+    print(f"{angka} adalah bilangan ganjil. ")
+
+Terminal
+
+PS C:\Users\Gagah> & C:/Users/Gagah/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/Gagah/Kiky/BilanganGanjilatau genap.py"
+Masukkan angka: 4
+4 adalah bilangan genap. 
+PS C:\Users\Gagah> & C:/Users/Gagah/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/Gagah/Kiky/BilanganGanjilatau genap.py"
+Masukkan angka: 9
+9 adalah bilangan ganjil. 
+PS C:\Users\Gagah>        
