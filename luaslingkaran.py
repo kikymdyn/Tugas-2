fed72f167d5e11daf22c2289893ef5e6cd9fb466
Muhammad Ribqi Madian Putra <rifkymadianp@gmail.com>
2024-09-12 13:50:48 +0800

    Create luaslingkaran.py

---
+++ luaslingkaran.py
@@ -0,0 +1,11 @@
+import math 
+r = float(input("Masukkan jari jari lingkaran: ")) 
+luas = math.pi * r ** 2
+print(f"Luas lingkaran dengan jari jari {r} adalah {luas:.2f}")
+
+Terminal
+
+PS C:\Users\Gagah> & C:/Users/Gagah/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/Gagah/Kiky/Luas lingkaran,py2.py"
+Masukkan jari jari lingkaran: 6
+Luas lingkaran dengan jari jari 6.0 adalah 113.10
+PS C:\Users\Gagah> 
