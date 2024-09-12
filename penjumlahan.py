33df26accda80f305817ed8442281bb65684d131
Muhammad Ribqi Madian Putra <rifkymadianp@gmail.com>
2024-09-12 13:49:17 +0800

    Update penjumlahan.py

--- penjumlahan.py
+++ penjumlahan.py
@@ -1,1 +1,14 @@
+a = int(input("Masukkan bilangan a: "))
+b = int(input("Masukkan bilangan b: "))
+jumlah = a + b
+print("Jumlah", a, "+", b, "adalah: ", jumlah)
+
+
+
+Terminal
+PS C:\Users\Gagah> & C:/Users/Gagah/AppData/Local/Programs/Python/Python312/python.exe c:/Users/Gagah/Kiky/Penjumlahan.py1.py
+Masukkan bilangan a: 350
+Masukkan bilangan b: 50
+Jumlah 350 + 50 adalah:  400
+PS C:\Users\Gagah> 
 
